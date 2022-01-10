fe17952c2419635a9997e10358cf02bf34abae6a
Antti Mikkonen <antti20138@student.hamk.fi>
2022-01-10 22:02:40 +0200

    Add project files.

---
+++ Piirtaminen/MainWindow.xaml.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Media.Animation;
+
+namespace Piirtaminen
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            MyCanvas.SetCoordinates();
+            MyCanvas.InvalidateVisual();
+
+            Random rnd = new Random();
+
+            DoubleAnimation a = new DoubleAnimation();
+            a.From = Canvas.GetTop(musta);
+            a.To = rnd.Next(0, 320);
+            a.Duration = new Duration(TimeSpan.Parse("0:0:0.2"));
+            musta.BeginAnimation(Canvas.TopProperty, a);
+
+            DoubleAnimation b = new DoubleAnimation();
+            b.From = Canvas.GetTop(sininen);
+            b.To = rnd.Next(0, 320);
+            b.Duration = new Duration(TimeSpan.Parse("0:0:0.3"));
+            sininen.BeginAnimation(Canvas.TopProperty, b);
+
+            DoubleAnimation c = new DoubleAnimation();
+            c.From = Canvas.GetLeft(sininen);
+            c.To = rnd.Next(0, 320);
+            c.Duration = new Duration(TimeSpan.Parse("0:0:0.3"));
+            sininen.BeginAnimation(Canvas.LeftProperty, c);
+
+            DoubleAnimation d = new DoubleAnimation();
+            d.From = Canvas.GetTop(oranssi);
+            d.To = rnd.Next(0, 320);
+            d.Duration = new Duration(TimeSpan.Parse("0:0:0.4"));
+            oranssi.BeginAnimation(Canvas.TopProperty, d);
+
+            DoubleAnimation f = new DoubleAnimation();
+            f.From = Canvas.GetLeft(oranssi);
+            f.To = rnd.Next(0, 320);
+            f.Duration = new Duration(TimeSpan.Parse("0:0:0.4"));
+            oranssi.BeginAnimation(Canvas.LeftProperty, f);
+
+            DoubleAnimation g = new DoubleAnimation();
+            g.From = Canvas.GetTop(vihrea);
+            g.To = rnd.Next(0, 320);
+            g.Duration = new Duration(TimeSpan.Parse("0:0:0.5"));
+            vihrea.BeginAnimation(Canvas.TopProperty, g);
+
+            DoubleAnimation h = new DoubleAnimation();
+            h.From = Canvas.GetLeft(vihrea);
+            h.To = rnd.Next(0, 320);
+            h.Duration = new Duration(TimeSpan.Parse("0:0:0.5"));
+            vihrea.BeginAnimation(Canvas.LeftProperty, h);
+
+            DoubleAnimation i = new DoubleAnimation();
+            i.From = Canvas.GetTop(punainen);
+            i.To = rnd.Next(0, 320);
+            i.Duration = new Duration(TimeSpan.Parse("0:0:0.6"));
+            punainen.BeginAnimation(Canvas.TopProperty, i);
+
+            DoubleAnimation j = new DoubleAnimation();
+            j.From = Canvas.GetLeft(punainen);
+            j.To = rnd.Next(0, 320);
+            j.Duration = new Duration(TimeSpan.Parse("0:0:0.6"));
+            punainen.BeginAnimation(Canvas.LeftProperty, j);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            
+        }
+    }
+}
---
+++ Piirtaminen/MyCanvas.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Piirtaminen
+{
+    internal class MyCanvas : Canvas
+    {
+        double a = 110;
+        double b = 200;
+        double c = 155;
+        double d = 240;
+        double f = 200;
+        double g = 200;
+        double h = 245;
+        double i = 240;
+        double j = 290;
+        double k = 200;
+        
+
+
+        protected override void OnRender(DrawingContext dc)
+        {
+            //base.OnRender(dc);
+            dc.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Blue, 6), new System.Windows.Point(a, b), 37, 37);
+            dc.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Orange, 6), new System.Windows.Point(c, d), 37, 37);
+            dc.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Black, 6), new System.Windows.Point(f, g), 37,37);
+            dc.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Green, 6), new System.Windows.Point(h, i), 37, 37);
+            dc.DrawEllipse(Brushes.Transparent, new Pen(Brushes.Red, 6), new System.Windows.Point(j, k), 37, 37);
+                        
+        }
+
+        public void SetCoordinates()
+        {
+            Random rnd = new Random();
+            var minx = 20;
+            var maxx = 339;
+            var miny = 37;
+            var maxy = 370;
+
+            this.a = rnd.Next(minx, maxx);
+            this.b = rnd.Next(miny, maxy);
+            this.c = rnd.Next(minx, maxx);
+            this.d = rnd.Next(miny, maxy);
+            this.f = rnd.Next(minx, maxx);
+            this.g = rnd.Next(miny, maxy);
+            this.h = rnd.Next(minx, maxx);
+            this.i = rnd.Next(miny, maxy);
+            this.j = rnd.Next(minx, maxx);
+            this.k = rnd.Next(miny, maxy);
+
+
+
+
+        }
+
+
+    }
+}
